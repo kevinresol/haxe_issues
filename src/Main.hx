3332ea86e7cdaa1a8f106691753f8bb748aaf0c1
Kevin Leung <kevinresol@gmail.com>
2019-04-23 17:08:09 +0800

    ok

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,19 @@
-package;
-
 class Main {
 	static function main() {
-		
+		var a:Abstract = new Impl();
+		trace(a.foo.bind()());
 	}
+}
+
+class Impl implements Interface {
+	public function new() {}
+	public function foo():String return 'foo';
+}
+
+@:forward
+abstract Abstract(Interface) from Interface to Interface {}
+
+interface Interface {
+	// @:keep // uncomment this to "fix"
+	function foo():String;
 }
