500d7523984147912dca9d992b9b97ba1ebd6df4
Kevin Leung <kevinresol@gmail.com>
2021-01-08 15:27:49 +0800

    For https://github.com/haxetink/tink_macro/issues/34

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,23 @@
-package;
+import tink.pure.Vector;
+import tink.pure.List;
+import coconut.ui.View;
 
-class Main {
-	static function main() {
-		
-	}
+class Main extends View {
+	static function main() {}
+	
+	static var data:List<Bar> = null;
+	
+	function render() '
+		<Foo data=${Vector.fromIterable(data)}/>
+	';
+}
+
+class Foo<T> extends View {
+	@:attr var data:Vector<T>;
+	
+	function render() '<div/>';
+}
+
+typedef Bar = {
+	final x:Int;
 }
